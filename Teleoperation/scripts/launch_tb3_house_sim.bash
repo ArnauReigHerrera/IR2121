4eb2b8426ecfbf49241bef8f04d22771f9c7dca2
ArnauReigHerrera <al416033@uji.es>
2023-09-11 18:58:26 +0200

    Update launch_tb3_house_sim.bash

--- Teleoperation/scripts/launch_tb3_house_sim.bash
+++ Teleoperation/scripts/launch_tb3_house_sim.bash
@@ -2,6 +2,6 @@
 source /opt/ros/foxy/setup.bash
 export ROS_LOCALHOST_ONLY=1
 export TURTLEBOT3_MODEL=burger
-ros2 lauch turtlebot3_gazebo \
+ros2 launch turtlebot3_gazebo \
      turtlebot3_house.launch.py
 
