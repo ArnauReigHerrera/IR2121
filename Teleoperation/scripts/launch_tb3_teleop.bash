67b37c3cd0db6943b2f3fb2789187ff99b5c8ce1
ArnauReigHerrera <al416033@uji.es>
2023-09-11 19:10:19 +0200

    Create launch_tb3_teleop.bash

---
+++ Teleoperation/scripts/launch_tb3_teleop.bash
@@ -0,0 +1,16 @@
+Control Your TurtleBot3!
+---------------------------
+Moving around:
+        w
+   a    s    d
+        x
+
+w/x : increase/decrease linear velocity 
+(Burger : ~ 0.22, Waffle Pi : ~ 0.26)
+a/d : increase/decrease angular velocity 
+(Burger : ~ 2.84, Waffle Pi : ~ 1.82)
+
+space key, s : force stop
+
+CTRL-C to quit
+
