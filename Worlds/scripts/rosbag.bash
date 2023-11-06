afbba1f159a8e1abad5122c845f1ee55eb6f27d6
ArnauReigHerrera <>
2023-11-06 19:01:49 +0100

    practicas 6 nov

--- Worlds/scripts/rosbag.bash
+++ Worlds/scripts/rosbag.bash
@@ -2,4 +2,5 @@
 source /opt/ros/foxy/setup.bash
 export TURTLEBOT3_MODEL=burger
 export ROS_LOCALHOST_ONLY=1
-ros2 bag record /clock /map /scan /odom /tf /tf_static /amcl_pose /particlecloud /robot_description
+ros2 bag record /clock /map /odom /robot_description /scan /scan_matched_points2 /submap_list /tf /tf_static
+
